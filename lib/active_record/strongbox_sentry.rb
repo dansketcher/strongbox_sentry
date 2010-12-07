074b8f438d183a548d90507efe8704591f33ed46
Dan Sketcher <dan@sketcher.net>
2010-12-07 15:36:41 +1000

    Add a hook for hanging audit logging for decryption of data

--- lib/active_record/strongbox_sentry.rb
+++ lib/active_record/strongbox_sentry.rb
@@ -22,7 +22,7 @@ module ActiveRecord # :nodoc:
           # Strongbox returns the string "*encrypted*" if the field is still
           # locked - make this more Sentry-like by returning nil instead
           out = nil if out == "*encrypted*"
-          decrypted_values[temp_sentry.attr_name] = out
+          set_decrypted_value(temp_sentry.attr_name, out)
           out
         end
         
@@ -30,12 +30,17 @@ module ActiveRecord # :nodoc:
           send(temp_sentry.crypted_attr_name).instance_variable_get('@instance')[temp_sentry.crypted_attr_name.to_s] = value
           nil
         end
-      
+        
         define_method("#{temp_sentry.attr_name}=") do |value|
           decrypted_values[temp_sentry.attr_name] = value
           nil
         end
-      
+        
+        # Add a method for when the decrypted value is stored - for attachment of audit logging as necessary
+        define_method("set_decrypted_value") do |key, value|
+          decrypted_values[key] = value
+        end
+        
         private
         define_method(:decrypted_values) do
           @decrypted_values ||= {}
